1ed1f304badb484bcf6c1ebeb091caa4c577cefd
Rasul <rasul.pundogar@bluephenix.ph>
2023-02-15 14:49:19 +0800

    Update

--- scripts/script.js
+++ scripts/script.js
@@ -9,4 +9,23 @@ function ClickMe(){
     h1.style.color = "blue";
     h1.style.fontSize = "20px";
 
+
+    var x = 10;
+    var y = 20;
+
+    var z = x + y;
+    var title = "Blue Phenix Skills Training Center Corp."
+    h1.innerHTML = title;
+}
+
+function calculate(){
+
+    var firstNum = document.getElementById("firstNum").value;
+    var secondNum = document.getElementById("secondNum").value;
+
+    var result = parseInt(firstNum) + parseInt(secondNum);
+
+    var output = document.getElementById("result");
+
+    output.value = result;
 }
